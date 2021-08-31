8989a2503ff7e30ec8e072046c09c24c05fe3f48
Ramon <ramonzxz@icloud.com>
2021-08-31 19:45:11 -0300

    Inicio projeto

---
+++ vpc/src/main/java/com/br/vpc/VpcApplication.java
@@ -0,0 +1,13 @@
+package com.br.vpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VpcApplication.class, args);
+	}
+
+}
