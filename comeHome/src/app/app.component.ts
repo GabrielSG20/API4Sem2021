71d5c523c6540a3a30b0ccca8a99ea370ded7fbd
Gabriel Soares <61523979+GabrielSG20@users.noreply.github.com>
2021-09-16 20:52:15 -0300

    Teste funcional get-org front (#22)

--- comeHome/src/app/app.component.ts
+++ comeHome/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'comeHome';
+  public teste: any;
+  constructor(private appService: AppService) { }
+  ngOnInit(): void {
+    this.getOrgs();
+  }
+
+  private getOrgs(){
+    this.appService.getOrgs().subscribe((values) => {
+      this.teste = values;
+      console.log(this.teste);
+    });
+  }
 }
--- comeHome/src/app/app.module.ts
+++ comeHome/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { ViewLoginComponent } from './view-login/view-login.component';
 import { ViewRegisterOrgComponent } from './view-register-org/view-register-org.component';
@@ -14,6 +15,7 @@ import { ViewRegisterOrgComponent } from './view-register-org/view-register-org.
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
