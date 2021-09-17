345614a95556b36f7780d9fd42cdedd9bdd43085
ramon <ramonzxz@icloud.com>
2021-09-16 22:26:18 -0300

    conflitos

--- vpc/src/main/java/com/br/vpc/VpcApplication.java
+++ vpc/src/main/java/com/br/vpc/VpcApplication.java
@@ -1,5 +1,6 @@
 package com.br.vpc;
 
+import com.br.vpc.model.ConvidadosModel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,10 @@ public class VpcApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VpcApplication.class, args);
+
+		ConvidadosModel convidadosModel = new ConvidadosModel();
+		convidadosModel.setEmailConvidado("ramonzxz@gmail.com gabrielsoaresg@gmail.com");
+		convidadosModel.OneEmail();
 	}
 
 }
