3b20d974f2f784842039e15654c1175549036088
Mike Barcelos Batista <mbarcelos@img.com.br>
2021-10-06 21:47:54 -0300

    Tratamento dos dados

--- comeHome/src/app/view-events/view-events.component.ts
+++ comeHome/src/app/view-events/view-events.component.ts
@@ -59,24 +59,133 @@ export class ViewEventsComponent implements OnInit {
     action: string;
     event: CalendarEvent;
   };
-  actions: CalendarEventAction[] = [
-    {
-      label: '<i class="fas fa-fw fa-pencil-alt"></i>',
-      a11yLabel: 'Edit',
-      onClick: ({ event }: { event: CalendarEvent }): void => {
-        this.handleEvent('Edited', event);
-      },
-    },
-    {
-      label: '<i class="fas fa-fw fa-trash-alt"></i>',
-      a11yLabel: 'Delete',
-      onClick: ({ event }: { event: CalendarEvent }): void => {
-        this.events = this.events.filter((iEvent) => iEvent !== event);
-        this.handleEvent('Deleted', event);
-      },
-    },
-  ];
+  public elemento: any;
   refresh: Subject<any> = new Subject();
+  public data: Array<any> = [
+    {
+      "idEvento": 9,
+      "titulo": "Evento1",
+      "descricao": "Desc 1",
+      "dataInicio": "17/09/2021 08:00",
+      "dataEncerramento": "17/09/2021 12:00",
+      "tipoEvento": "fechado",
+      "status": null,
+      "imagemDivulgacao": null,
+      "org": {
+        "email": "teste@gmail.com",
+        "nomeCompleto": "Teste Name",
+        "cpf": "56781",
+        "telefone": "78961",
+        "departamento": null,
+        "nomeEmpresa": null,
+        "idOracle": null,
+        "comprovanteVacinacao": null,
+        "tipoUsuario": "interno",
+        "cargoUsuario": null,
+        "senhaUsuario": "pass123"
+      },
+      "nomeEspaco": [
+        {
+          "idEspaco": 2,
+          "nomeEspaco": "Open Space",
+          "capEspaco": 50
+        },
+        {
+          "idEspaco": 3,
+          "nomeEspaco": "Lounge on Hall",
+          "capEspaco": 20
+        }
+      ],
+      "fornecedores": [],
+      "convidados": [
+        {
+          "email": "mike@gmail.com",
+          "nomeCompleto": "Teste Name",
+          "cpf": "567815",
+          "telefone": "789615",
+          "departamento": null,
+          "nomeEmpresa": null,
+          "idOracle": null,
+          "comprovanteVacinacao": null,
+          "tipoUsuario": "interno",
+          "cargoUsuario": null,
+          "senhaUsuario": "pass1235"
+        },
+        {
+          "email": "ferraz@gmail.com",
+          "nomeCompleto": "Teste Name",
+          "cpf": "567812",
+          "telefone": "789612",
+          "departamento": null,
+          "nomeEmpresa": null,
+          "idOracle": null,
+          "comprovanteVacinacao": null,
+          "tipoUsuario": "interno",
+          "cargoUsuario": null,
+          "senhaUsuario": "pass1232"
+        }
+      ]
+    },
+    {
+      "idEvento": 10,
+      "titulo": "Evento1",
+      "descricao": "Desc 1",
+      "dataInicio": "17/09/2021 08:00",
+      "dataEncerramento": "17/09/2021 12:00",
+      "tipoEvento": "fechado",
+      "status": null,
+      "imagemDivulgacao": null,
+      "org": {
+        "email": "teste@gmail.com",
+        "nomeCompleto": "Teste Name",
+        "cpf": "56781",
+        "telefone": "78961",
+        "departamento": null,
+        "nomeEmpresa": null,
+        "idOracle": null,
+        "comprovanteVacinacao": null,
+        "tipoUsuario": "interno",
+        "cargoUsuario": null,
+        "senhaUsuario": "pass123"
+      },
+      "nomeEspaco": [
+        {
+          "idEspaco": 2,
+          "nomeEspaco": "Open Space",
+          "capEspaco": 50
+        },
+      ],
+      "fornecedores": [],
+      "convidados": [
+        {
+          "email": "mike@gmail.com",
+          "nomeCompleto": "Teste Name",
+          "cpf": "567815",
+          "telefone": "789615",
+          "departamento": null,
+          "nomeEmpresa": null,
+          "idOracle": null,
+          "comprovanteVacinacao": null,
+          "tipoUsuario": "interno",
+          "cargoUsuario": null,
+          "senhaUsuario": "pass1235"
+        },
+        {
+          "email": "ferraz@gmail.com",
+          "nomeCompleto": "Teste Name",
+          "cpf": "567812",
+          "telefone": "789612",
+          "departamento": null,
+          "nomeEmpresa": null,
+          "idOracle": null,
+          "comprovanteVacinacao": null,
+          "tipoUsuario": "interno",
+          "cargoUsuario": null,
+          "senhaUsuario": "pass1232"
+        }
+      ]
+    },
+  ]  
   events: CalendarEvent[] = [
     {
       start: setHours(setMinutes(new Date('2021-10-04'), 45), 12),
@@ -91,10 +200,10 @@ export class ViewEventsComponent implements OnInit {
       color: colors.blue,
     },
     {
-      start: setHours(setMinutes(new Date('2021-11-05'), 45), 12),
+      start: setHours(setMinutes(new Date('2021-10-06'), 45), 12),
       end: setHours(new Date('2021-10-06'), 18),
       title: 'Evento de teste2',
-      color: colors.yellow,
+      color: colors.blue,
     },
     {
       start: setHours(setMinutes(new Date('2021-12-05'), 45), 12),
@@ -109,8 +218,54 @@ export class ViewEventsComponent implements OnInit {
   constructor(private modal: NgbModal, public formBuilder: FormBuilder,) {}
 
   ngOnInit(): void {
+    this.eventPlot();
   }
 
+  colorEvent(element: any) {
+    let color;
+    if (element.nomeEspaco.length == 2) {
+      return colors.yellow;
+    }
+    else {
+      if(element.nomeEspaco[0].nomeEspaco == "Open Space") {
+        return colors.blue;
+      }
+      else {
+        return colors.red;
+      }
+    }
+  }
+  eventDate(element: any) {
+    element = element.split(" ", 1);
+    element = element.join();
+    element = element.split("/");
+    element = element.reverse();
+    let number = Number(element[2]);
+    number += 1;
+    element[2] = number.toString();
+    element = element.join('-');
+    return element;
+  }
+  eventHour(element: any) {
+    element = element.split(" ");
+    element = element[1];
+    element = element.split(":");
+    let number = Number(element[0]);
+    return number
+  }
+  eventPlot() {
+    for (var element of this.data) {
+      this.elemento = element;
+      var newEvent = {
+        start: setHours(new Date(this.eventDate(this.elemento.dataInicio)), this.eventHour(this.elemento.dataInicio)),
+        end: setHours(new Date(this.eventDate(this.elemento.dataEncerramento)), this.eventHour(this.elemento.dataEncerramento)),
+        title: this.elemento.titulo,
+        color: this.colorEvent(this.elemento),
+      };
+      this.events.push(newEvent);
+      console.log(this.elemento);
+    }
+  }
   dayClicked({ date, events }: { date: Date; events: CalendarEvent[] }): void {
     if (isSameMonth(date, this.viewDate)) {
       if (
@@ -122,6 +277,8 @@ export class ViewEventsComponent implements OnInit {
         this.activeDayIsOpen = true;
       }
       this.viewDate = date;
+      this.date = new FormControl(new Date(date));
+      
     }
   }
 
