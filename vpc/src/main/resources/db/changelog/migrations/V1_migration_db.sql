4679e424f9909e802e58a0c7253d7301da7e16bb
Gabriel Henrique de Souza Ferraz <bi.henriquesf@hotmail.com>
2021-11-12 21:36:37 -0300

    Branch nova

--- vpc/src/main/resources/db/changelog/migrations/V1_migration_db.sql
+++ vpc/src/main/resources/db/changelog/migrations/V1_migration_db.sql
@@ -1,8 +1,9 @@
---liquibase formatted sql
---changeset Gabriel Ferraz:1
+liquibase formatted sql
+Adicionando um sequence no banco de dados e inserindo alguns valores padrões dentro da tabela Espaco
+changeset Gabriel Ferraz:2
 
---CREATE SEQUENCE id_chave_seq;
+CREATE SEQUENCE id_chave_seq;
 
---changeset Soares:2
---INSERT INTO Espaco VALUES (ID_CHAVE_SEQ.nextval,50,'Open Space');
---INSERT INTO Espaco VALUES (ID_CHAVE_SEQ.nextval,20,'Lounge on Hall');
+changeset Soares:3
+INSERT INTO Espaco VALUES (ID_CHAVE_SEQ.nextval,50,'Open Space');
+INSERT INTO Espaco VALUES (ID_CHAVE_SEQ.nextval,20,'Lounge on Hall');
