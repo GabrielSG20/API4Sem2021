53f55329cd18411455c731fe1cbc7c93ddca0824
Gabriel Henrique de Souza Ferraz <bi.henriquesf@hotmail.com>
2021-12-01 19:52:15 -0300

    GET E POST Espaços

--- vpc/src/main/java/com/br/vpc/controller/EspacoController.java
+++ vpc/src/main/java/com/br/vpc/controller/EspacoController.java
@@ -1,10 +1,19 @@
 package com.br.vpc.controller;
 
+import com.br.vpc.model.EspacoModel;
+import com.br.vpc.model.EventoModel;
 import com.br.vpc.repository.EspacoRepository;
 import com.br.vpc.service.EspacoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping(value = "/espaco")
@@ -12,4 +21,17 @@ public class EspacoController {
 
     @Autowired
     EspacoService espacoService;
+
+    @PutMapping("/atualizar")
+    public ResponseEntity<Void> atualizarCap(@PathVariable Integer cap1, @PathVariable Integer cap2) {
+        espacoService.atualizarEspaco(cap1,cap2);
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping("/espacos")
+    public ResponseEntity<Void> listarespacos(@PathVariable String nome) {
+        Integer capEsp = espacoService.listarEspaco(nome);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
 }
